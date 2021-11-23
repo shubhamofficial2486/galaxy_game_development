c7fa2d16063a681619440e3417ac00eb8a258c8b
shubhamofficial2486 <45324404+shubhamofficial2486@users.noreply.github.com>
2021-11-23 12:30:56 +0530

    Add files via upload

---
+++ removed.py
@@ -0,0 +1,21 @@
+def on_parent(self, widget, parent):
+    # print("ON SIZE : H: " + str(self.width) + "H : " + str(self.height))
+    pass
+
+def on_size(self, *args):
+    # print("ON SIZE : H: "+ str(self.width)+ "H : "+ str(self.height))
+    # self.perspective_point_x = self.width/2
+    # self.perspective_point_y = self.height*0.75
+    # self.update_vertical_lines()
+    # self.update_horizontal_lines()
+    pass
+
+
+def on_perspective_point_x(self, widget, value):
+    # print("PX " + str(value))
+    pass
+
+
+def on_perspective_point_y(self, widget, value):
+    # print("PY " + str(value))
+    pass
---
+++ user_actions.py
@@ -0,0 +1,39 @@
+from kivy.uix.relativelayout import RelativeLayout
+
+
+def on_keyboard_down(self, keyboard, keycode, text, modifiers):
+    if keycode[1] == 'left':
+        self.current_speed_x = self.SPEED_x
+
+    elif keycode[1] == 'right':
+        self.current_speed_x = -self.SPEED_x
+    return True
+
+
+def on_keyboard_up(self, keyboard, keycode):
+    self.current_speed_x = 0
+    return True
+
+
+def on_touch_down(self, touch):
+
+    if not self.state_game_over and self.state_game_has_started:
+        if touch.x < self.width / 2:
+        #print('<-')
+            self.current_speed_x = self.SPEED_x
+
+        else:
+            #print('->')
+            self.current_speed_x = -self.SPEED_x
+
+    return  super(RelativeLayout, self).on_touch_down(touch)
+
+
+def on_touch_up(self, touch):
+    #print('up')
+    self.current_speed_x = 0
+
+def keyboard_closed(self):
+    self.keyboard.unbind(on_key_down=self.on_keyboard_down)
+    self.keyboard.unbind(on_key_up=self.on_keyboard_up)
+    self.keyboard = None
